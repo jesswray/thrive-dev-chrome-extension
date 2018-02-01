ef464f07d7769f8d31eccf2388912178d88a9607
Jessica Wray <jessica@thrivetrm.com>
2018-02-01 15:47:42 -0500

    Update for new environment urls

--- content.js
+++ content.js
@@ -1,21 +1,13 @@
 function turnColor(color){
   $('.nav-main-container').css('background-color', color);
-  };
+};
 
 function production(){
   turnColor('#FF0000');
 };
 
 function staging(){
-  turnColor('#D290D4');
-};
-
-function beta(){
-  turnColor('#6BB7FA');
-};
-
-function demo(){
-  turnColor('#FAAE6B');
+  turnColor('#b5f441');
 };
 
 var url = window.location.href
@@ -23,15 +15,9 @@ function colorChoice(){
   if ( url.search("staging") != -1 ){
     staging();
   }
-  else if ( url.search("beta") != -1 ){
-    beta();
-  }
-  else if ( url.search("demo") != -1 ){
-    demo();
-  }
-  else {
+  else if ( url.search("thrivetrm") != -1 ){
     production();
-  };
+  }
 };
 
 colorChoice();
